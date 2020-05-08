14032fcba1095e05f6ae2df954a4e9cbcf89e3aa
ksdsk <soonmoc.kang@gmail.com>
2020-05-08 00:15:54 -0300

    Donation Page Button Added

---
+++ js/charities.js
@@ -0,0 +1,11 @@
+function list1() {
+    window.location="https://en.horizonnb.ca"
+}
+
+function list2() {
+    window.location="https://www.youtube.com"
+}
+
+function list3() {
+    window.location="https://www.instagram.com"
+}
