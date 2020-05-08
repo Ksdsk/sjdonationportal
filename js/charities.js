85bc0ef336665d03c8d3648adab1360059d7e72b
ksdsk <soonmoc.kang@gmail.com>
2020-05-08 15:22:47 -0300

    May 8th

--- js/charities.js
+++ js/charities.js
@@ -1,11 +1,27 @@
 function list1() {
-    window.location="https://en.horizonnb.ca"
+    window.location="charitynamehere-1.html"
 }
-
 function list2() {
-    window.location="https://www.youtube.com"
+    window.location="charitynamehere-2.html"
 }
-
 function list3() {
-    window.location="https://www.instagram.com"
+    window.location="charitynamehere-3.html"
+}
+function list4() {
+    window.location="charitynamehere-4.html"
+}
+function list5() {
+    window.location="charitynamehere-5.html"
+}
+function list6() {
+    window.location="charitynamehere-6.html"
+}
+function list7() {
+    window.location="charitynamehere-7.html"
+}
+function list8() {
+    window.location="charitynamehere-8.html"
+}
+function list9() {
+    window.location="charitynamehere-9.html"
 }
