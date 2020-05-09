099977d62d8ecb297b34c9335b2e7542e351a049
ksdsk <soonmoc.kang@gmail.com>
2020-05-09 11:31:19 -0300

    Removed HTML extension

--- js/charities.js
+++ js/charities.js
@@ -1,27 +1,27 @@
 function list1() {
-    window.location="charitynamehere-1.html"
+    window.location="charitynamehere-1"
 }
 function list2() {
-    window.location="charitynamehere-2.html"
+    window.location="charitynamehere-2"
 }
 function list3() {
-    window.location="charitynamehere-3.html"
+    window.location="charitynamehere-3"
 }
 function list4() {
-    window.location="charitynamehere-4.html"
+    window.location="charitynamehere-4"
 }
 function list5() {
-    window.location="charitynamehere-5.html"
+    window.location="charitynamehere-5"
 }
 function list6() {
-    window.location="charitynamehere-6.html"
+    window.location="charitynamehere-6"
 }
 function list7() {
-    window.location="charitynamehere-7.html"
+    window.location="charitynamehere-7"
 }
 function list8() {
-    window.location="charitynamehere-8.html"
+    window.location="charitynamehere-8"
 }
 function list9() {
-    window.location="charitynamehere-9.html"
+    window.location="charitynamehere-9"
 }
