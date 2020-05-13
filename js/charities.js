f0abcc307a40067b2605ac4cc0c62933f8e96b7e
ksdsk <soonmoc.kang@gmail.com>
2020-05-13 13:54:44 -0300

    added CHARITIES folder

--- js/charities.js
+++ js/charities.js
@@ -2,26 +2,26 @@ function list1() {
     window.location="charities/sjbbbs.html"
 }
 function list2() {
-    window.location="charitynamehere-2"
+    window.location="charities/charitynamehere-2"
 }
 function list3() {
-    window.location="charitynamehere-3"
+    window.location="charities/charitynamehere-3"
 }
 function list4() {
-    window.location="charitynamehere-4"
+    window.location="charities/charitynamehere-4"
 }
 function list5() {
-    window.location="charitynamehere-5"
+    window.location="charities/charitynamehere-5"
 }
 function list6() {
-    window.location="charitynamehere-6"
+    window.location="charities/charitynamehere-6"
 }
 function list7() {
-    window.location="charitynamehere-7"
+    window.location="charities/charitynamehere-7"
 }
 function list8() {
-    window.location="charitynamehere-8"
+    window.location="charities/charitynamehere-8"
 }
 function list9() {
-    window.location="charitynamehere-9"
+    window.location="charities/charitynamehere-9"
 }
