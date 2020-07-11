f8e83b34b53ed921e04c1fac26a47eec29a4b92b
ksdsk <soonmoc.kang@gmail.com>
2020-07-11 12:32:08 -0300

    Volunteer skeleton

---
+++ donation/js/charities.js
@@ -0,0 +1,30 @@
+function list1() {
+    window.location="charities/sjbbbs"
+}
+function list2() {
+    window.location="charities/trc"
+}
+function list3() {
+    window.location="charities/horizonhealth"
+}
+function list4() {
+    window.location="charities/thesalvationarmy"
+}
+function list5() {
+    window.location="charities/make-a-wish-atlantic"
+}
+function list6() {
+    window.location="charities/kvoasis"
+}
+function list7() {
+    window.location="charities/georgefloyd"
+}
+function list8() {
+    window.location="charities/hamptonfoodbasket"
+}
+function list9() {
+    window.location="charities/hestiahouse"
+}
+function list10() {
+    window.location="charities/habitatforhumanity"
+}
